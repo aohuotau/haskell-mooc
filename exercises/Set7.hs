7de0bdbc4cf2c3f30d087bcda0b81f1858a47a14
Huotari Antti Olavi <antti.huotari10@gmail.com>
2024-04-08 16:34:52 +0300

    7 tehty

--- exercises/Set7.hs
+++ exercises/Set7.hs
@@ -26,11 +26,11 @@ data Velocity = Velocity Double
 
 -- velocity computes a velocity given a distance and a time
 velocity :: Distance -> Time -> Velocity
-velocity = todo
+velocity (Distance d) (Time t) = Velocity (d / t)
 
 -- travel computes a distance given a velocity and a time
 travel :: Velocity -> Time -> Distance
-travel = todo
+travel (Velocity v) (Time t) = Distance (v * t)
 
 ------------------------------------------------------------------------------
 -- Ex 2: let's implement a simple Set datatype. A Set is a list of
@@ -49,15 +49,15 @@ data Set a = Set [a]
 
 -- emptySet is a set with no elements
 emptySet :: Set a
-emptySet = todo
+emptySet = Set []
 
 -- member tests if an element is in a set
 member :: Eq a => a -> Set a -> Bool
-member = todo
+member a (Set xs) = a `elem` xs
 
 -- add a member to a set
-add :: a -> Set a -> Set a
-add = todo
+add :: Ord a => a -> Set a -> Set a
+add a (Set xs) = Set (sort(nub(a:xs)))
 
 ------------------------------------------------------------------------------
 -- Ex 3: a state machine for baking a cake. The type Event represents
@@ -92,10 +92,21 @@ add = todo
 data Event = AddEggs | AddFlour | AddSugar | Mix | Bake
   deriving (Eq,Show)
 
-data State = Start | Error | Finished
+data State = Start | AddedEggs | AddedFlour | AddedSugar | AddedFlourSugar | Mixed | Error | Finished
   deriving (Eq,Show)
 
-step = todo
+
+step :: State -> Event -> State
+step state e = case (state, e) of
+    (Start, AddEggs) -> AddedEggs
+    (AddedEggs, AddFlour) -> AddedFlour
+    (AddedEggs, AddSugar) -> AddedSugar
+    (AddedFlour, AddSugar) -> AddedFlourSugar
+    (AddedSugar, AddFlour) -> AddedFlourSugar
+    (AddedFlourSugar, Mix) -> Mixed
+    (Mixed, Bake) -> Finished
+    (Finished, _) -> Finished 
+    _ -> Error
 
 -- do not edit this
 bake :: [Event] -> State
@@ -115,7 +126,9 @@ bake events = go Start events
 --   average (1.0 :| [2.0,3.0])  ==>  2.0
 
 average :: Fractional a => NonEmpty a -> a
-average = todo
+average (x :| []) = x
+average (x :| xs) = (x + sum xs) / fromIntegral(length xs + 1)
+
 
 ------------------------------------------------------------------------------
 -- Ex 5: reverse a NonEmpty list.
@@ -123,7 +136,10 @@ average = todo
 -- PS. The Data.List.NonEmpty type has been imported for you
 
 reverseNonEmpty :: NonEmpty a -> NonEmpty a
-reverseNonEmpty = todo
+reverseNonEmpty (x :| xs) = case Prelude.reverse xs of
+                          [] -> x :| []
+                          (y:ys) -> y :| (ys ++ [x]) 
+
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement Semigroup instances for the Distance, Time and
@@ -136,6 +152,16 @@ reverseNonEmpty = todo
 --    ==> Velocity 20
 
 
+instance Semigroup Distance where
+  (Distance a) <> (Distance b) = Distance (a + b)
+
+instance Semigroup Time where
+  (Time a) <> (Time b) = Time (a + b)
+
+instance Semigroup Velocity where
+  (Velocity a) <> (Velocity b) = Velocity (a + b)
+
+
 ------------------------------------------------------------------------------
 -- Ex 7: implement a Monoid instance for the Set type from exercise 2.
 -- The (<>) operation should be the union of sets.
@@ -144,6 +170,18 @@ reverseNonEmpty = todo
 --
 -- What are the class constraints for the instances?
 
+instance Ord a => Monoid (Set a) where
+  mempty = emptySet
+
+instance Ord a => Semigroup (Set a) where
+    (Set xs) <> (Set ys) = Set (merge xs ys)
+      where
+        merge [] ys = ys
+        merge xs [] = xs
+        merge (x:xs) (y:ys)
+          | x < y = x : merge xs (y:ys)
+          | x == y = x : merge xs ys
+          | otherwise = y : merge (x:xs) ys
 
 ------------------------------------------------------------------------------
 -- Ex 8: below you'll find two different ways of representing
@@ -166,28 +204,46 @@ reverseNonEmpty = todo
 
 data Operation1 = Add1 Int Int
                 | Subtract1 Int Int
+                | Multiply1 Int Int
   deriving Show
 
 compute1 :: Operation1 -> Int
 compute1 (Add1 i j) = i+j
 compute1 (Subtract1 i j) = i-j
+compute1 (Multiply1 i j) = i*j
 
 show1 :: Operation1 -> String
-show1 = todo
+show1 operation = case operation of
+  Add1 i j -> show i ++ "+" ++ show j
+  Subtract1 i j -> show i ++ "-" ++ show j
+  Multiply1 i j -> show i ++ "*" ++ show j
 
 data Add2 = Add2 Int Int
   deriving Show
 data Subtract2 = Subtract2 Int Int
   deriving Show
+data Multiply2 = Multiply2 Int Int
+  deriving Show
 
-class Operation2 op where
-  compute2 :: op -> Int
+class Operation2 a where
+  compute2 :: a -> Int
+  show2 :: a -> String
 
 instance Operation2 Add2 where
   compute2 (Add2 i j) = i+j
+  show2 (Add2 i j) = show i ++ "+" ++ show j
 
 instance Operation2 Subtract2 where
   compute2 (Subtract2 i j) = i-j
+  show2 (Subtract2 i j) = show i ++ "-" ++ show j
+
+instance Operation2 Multiply2 where
+  compute2 (Multiply2 i j) = i*j
+  show2 (Multiply2 i j) = show i ++ "*" ++ show j
+
+
+
+
 
 
 ------------------------------------------------------------------------------
